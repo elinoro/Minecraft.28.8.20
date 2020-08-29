cbceb629b7cbf8322bfcb1ec147b68bcd7c3f024
elinoro <o.elinor.91@gmail.com>
2020-08-30 00:54:21 +0300

    update

---
+++ script.js
@@ -0,0 +1,72 @@
+let matrix = 
+[
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 6, 6, 6, 6, 6, 0, 6, 6, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 6, 6, 6, 6, 6, 6, 6, 6, 6, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 0, 0, 0, 0, 0, 3, 3, 3, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0],
+  [0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0],
+  [0, 0, 0, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 4, 4, 0, 5, 0, 0, 4],
+  [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+];
+
+//0 = sky
+//1 = dirt
+//2 = dirt+grass on top
+//3 = bush
+//4 = rock
+//5 = tree trunk
+//6 = clouds
+
+let world = document.querySelector('.world');
+
+for(let row = 0; row < matrix.length; row++){
+  for(let col = 0; col < matrix[row].length; col++){
+    let tile = document.createElement('div');
+    tile.classList.add('tile');
+
+    switch(matrix[row][col]){
+      case 0:
+        tile.classList.add('sky');
+        break;
+      case 1:
+        tile.classList.add('soil');
+        break;
+      case 2:
+        tile.classList.add('soilGrass');
+        break;
+      case 3:
+        tile.classList.add('bush');
+        break;
+      case 4:
+        tile.classList.add('rock');
+        break;
+      case 5:
+        tile.classList.add('trunk');
+        break;
+      case 6:
+        tile.classList.add('cloud');
+        break;
+    }
+    world.appendChild(tile);
+  }
+}
+
+// let tiles = document.querySelectorAll('.tile');
+// tiles.forEach((tile) => {
+//   tile.addEventListener('click', (event) => {
+//     console.log(event.currentTarget.classList.length);
+//   })
+// })
